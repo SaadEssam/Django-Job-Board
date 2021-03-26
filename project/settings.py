7ab82ab1a855ed4d904012065435b6b8423b3c70
Saad Essam <77013858+SaadEssam@users.noreply.github.com>
2021-03-26 22:02:07 +0200

    Error Registration/Login

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+# import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 
     # My Apps
     'job',
+    "accounts",
     "bootstrap4",
 
 ]
@@ -59,6 +62,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': ['tamplates'],
         'APP_DIRS': True,
         'OPTIONS': {
