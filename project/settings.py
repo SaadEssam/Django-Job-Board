85689ffc71f933b9d33b5485786985bfd67c3d22
Saad Essam <77013858+SaadEssam@users.noreply.github.com>
2021-06-30 21:01:49 +0200

    add signup form

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
 
     # My Apps
     'job',
-    "accounts",
     "bootstrap4",
 
 ]
