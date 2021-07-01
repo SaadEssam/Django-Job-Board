8bcf7be7ec14316ef82e66ff0ef7126846ceb5bf
Saad Essam <77013858+SaadEssam@users.noreply.github.com>
2021-07-01 16:46:15 +0200

    add Profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
 
 
 def signup(request):
     pass
+
+
+
+
+
+
+
+
+
+
+def profile(request):
+    profile = profile.objects.get(user=request.user)
+    return render(request, 'accounts/profile.html',{'profile': profile})
+
+
+def profile_edit(request):
+    
+    return render(request, 'accounts/profile_edit.html',{})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
